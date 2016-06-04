5a0e21cba405490d6d7b50123288a69be207b00a
gkueny <gaetankueny@gmail.com>
2016-06-04 18:00:09 +0200

    add class to compile

--- DependencyInjection/GKHtmlMetaCrawlerExtension.php
+++ DependencyInjection/GKHtmlMetaCrawlerExtension.php
@@ -24,5 +24,9 @@ class GKHtmlMetaCrawlerExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $this->addClassesToCompile(array(
+            'GK\\HtmlMetaCrawlerBundle\\Services\\MetaCrawler'
+        ));
     }
 }
