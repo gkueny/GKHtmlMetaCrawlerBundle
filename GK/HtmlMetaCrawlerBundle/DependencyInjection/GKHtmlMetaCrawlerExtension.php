94a28118f80c1cb97a490f896b4db146a3d1b456
gkueny <gaetankueny@gmail.com>
2016-06-04 17:23:36 +0200

    update dependency injection

--- GK/HtmlMetaCrawlerBundle/DependencyInjection/GKHtmlMetaCrawlerExtension.php
+++ GK/HtmlMetaCrawlerBundle/DependencyInjection/GKHtmlMetaCrawlerExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class GKHtmlCrawlerExtension extends Extension
+class GKHtmlMetaCrawlerExtension extends Extension
 {
     /**
      * {@inheritdoc}
