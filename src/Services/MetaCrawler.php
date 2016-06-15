49bc8b4a939cede29ac77e21718e47446ea73266
gkueny <gaetankueny@gmail.com>
2016-06-15 21:29:57 +0200

    update service, use curl to get web page + throw exception on error

--- src/Services/MetaCrawler.php
+++ src/Services/MetaCrawler.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace GK\HtmlMetaCrawlerBundle\Services;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 /**
  * Class MetaCrawler
@@ -13,7 +14,7 @@ class MetaCrawler
     public function getAllMeta($url, $followRedirect = false)
     {
 
-        
+
 
         $response = $this->getBasicMeta($url, $followRedirect);
 
@@ -129,28 +130,66 @@ class MetaCrawler
         //disable html5 error
         libxml_use_internal_errors(true);
 
-        // set context
-        $context = stream_context_create(array(
-                'http' => array(
-                    'follow_location' => $followRedirect
-                )
-            ));
+        $webPage = $this->get_web_page($url, $followRedirect);
 
-
-        //get page by url
-        $page = @file_get_contents($url, false, $context);
+        $page = $webPage["content"] ;
 
         if (!$page) {
-            return false;
+            throw new Exception( $webPage['errno'] . " : " . $webPage['errmsg']);
         }
 
         //create simplexml object
         $doc = new \DOMDocument();
+
         $doc->loadHTML($page);
         $xml = simplexml_import_dom($doc); // just to make xpath more simple
 
         libxml_use_internal_errors(false);
 
         return $xml;
+    }
+
+    /**
+     * Get a web file (HTML, XHTML, XML, image, etc.) from a URL.  Return an
+     * array containing the HTTP server response header fields and content.
+     *
+     * @param $url
+     * @param $followRedirect
+     *
+     * @return mixed
+     */
+    private function get_web_page( $url , $followRedirect)
+    {
+        $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
+
+        $options = array(
+            CURLOPT_CUSTOMREQUEST  =>"GET",             //set request type post or get
+            CURLOPT_POST           =>false,             //set to GET
+            CURLOPT_USERAGENT      => $user_agent,      //set user agent
+            CURLOPT_COOKIEFILE     =>"cookie.txt",      //set cookie file
+            CURLOPT_COOKIEJAR      =>"cookie.txt",      //set cookie jar
+            CURLOPT_RETURNTRANSFER => true,             // return web page
+            CURLOPT_HEADER         => false,            // don't return headers
+            CURLOPT_FOLLOWLOCATION => $followRedirect,  // follow redirects
+            CURLOPT_ENCODING       => "",               // handle all encodings
+            CURLOPT_AUTOREFERER    => true,             // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,              // timeout on connect
+            CURLOPT_TIMEOUT        => 120,              // timeout on response
+            CURLOPT_MAXREDIRS      => 10,               // stop after 10 redirects
+        );
+
+        $ch      = curl_init( $url );
+        curl_setopt_array( $ch, $options );
+        $content = curl_exec( $ch );
+        $err     = curl_errno( $ch );
+        $errmsg  = curl_error( $ch );
+        $header  = curl_getinfo( $ch );
+        curl_close( $ch );
+
+        $header['errno']   = $err;
+        $header['errmsg']  = $errmsg;
+        $header['content'] = $content;
+
+        return $header;
     }
 }
